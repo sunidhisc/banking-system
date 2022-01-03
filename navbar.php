2118bd296a5a77f0db5e4e5307da90544083fee6
sunidhisc <90034277+sunidhisc@users.noreply.github.com>
2022-01-03 14:10:11 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #0c1036" style="background-color: #007fc9"><b>ABC BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : ;"><b>HOME</b></a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #030b52;"><b>MONEY TRANSACTION</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #030b52;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+          </div>
+       </nav>
